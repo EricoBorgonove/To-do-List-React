d5ae4bb02ea58519501596446e8ee8a30aff5ae0
ericoborgonove <borgonove@gmail.com>
2025-08-12 14:03:13 -0400

    feat(start-projetct): commit inicial do início do projeto

---
+++ my-todo/src/components/Titulo.jsx
@@ -0,0 +1,6 @@
+const Titulo = ({texto}) =>{
+    return(
+        <h1 className="title">{texto}</h1>
+    )
+}
+export default Titulo
---
+++ my-todo/src/hooks/useLocalStorage.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export default function useLocalStorage (key, initialValue){
+    const [value, setValue] = useState(()=> {
+        try{
+            const raw = localStorage.getItem(key);
+            return (raw != null ? JSON.parse(raw) : initialValue)
+        }catch{
+            return initialValue
+        }
+    });
+    useEffect(()=>{
+        try{
+            localStorage.setItem(key, JSON.stringify(value))
+        }catch{
+            // ignora erros, 
+        }
+    },[key, value]);
+    return [value, setValue];
+}
