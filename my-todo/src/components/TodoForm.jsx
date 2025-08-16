a4d769990b034208ac8d482d1e64eab7bfc33d0f
ericoborgonove <borgonove@gmail.com>
2025-08-16 09:07:26 -0400

    feat(TodoForm): feature finalizada

--- my-todo/src/components/TodoForm.jsx
+++ my-todo/src/components/TodoForm.jsx
@@ -1,11 +1,20 @@
 import { useRef, useEffect } from "react";
 
-const TodoForm = () => {
+const TodoForm = ({onAdd}) => {
     const inputRef = useRef(null);
 
     useEffect(()=>{
-        inputRef.current?.fucus();
+        inputRef.current?.focus();
     },[]);
+
+    function handleSubmit (e){
+        e.preventDefault();
+        const text = inputRef.current.value.trim();
+        if(!text) return;
+        onAdd(text)
+        inputRef.current.value = "";
+        inputRef.current.focus();
+    }
     
     return (
         <form onSubmit={handleSubmit} className="form">
